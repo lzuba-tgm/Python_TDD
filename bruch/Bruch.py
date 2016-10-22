6e565823ec9fbaa0770d5579589b678641df9e21
Lukas Zuba <lzuba@student.tgm.ac.at>
2016-10-22 13:27:43 +0200

    Added add Function and some more tests passed

--- bruch/Bruch.py
+++ bruch/Bruch.py
@@ -18,8 +18,14 @@ class Bruch(object):
         else:
             self.nenner = 1
 
-    def _Bruch__makeBruch (value):
-        return value
+        if self.nenner == 0:
+            raise ZeroDivisionError
+        elif isinstance(self.nenner,float) or isinstance(self.zaehler,float) or isinstance(self.zaehler,str):
+            raise TypeError
+
+    @staticmethod
+    def __makeBruch (value):
+        return Bruch(value)
 
     def __abs__(self):
         return abs(float(self.zaehler) / float(self.nenner))
@@ -31,11 +37,64 @@ class Bruch(object):
         return float(self.zaehler) / float(self.nenner)
 
     def __str__(self):
+
+        if self.nenner == 1:
+            part = int(self.zaehler) / int(self.nenner)
+            print part
+            return '(%d)' % part
+
         if self.zaehler < 0:
             self.zaehler = -self.zaehler
         if self.nenner < 0:
             self.nenner = -self.nenner
         return "(%d/%d)" % (self.zaehler,self.nenner)
 
-    def __add__(self, other):
-        return Bruch(self.zaehler+other, self.nenner)
+    def __iadd__(self, other):
+        return self+other
+
+    def __invert__(self):
+        return Bruch(self.nenner,self.zaehler)
+
+    def __neg__(self):
+        return Bruch(-1 * self.zaehler,self.nenner)
+
+    def __pow__(self, power, modulo=None):
+        if isinstance(power, int):
+            return Bruch(self.zaehler ** power, self.nenner ** power)
+        else:
+            raise TypeError
+
+    def __add__(self, *args):
+        part = 0
+
+        if isinstance(args[0], int):
+            part += float(Bruch(self.zaehler, self.nenner)) + float(args[0])
+        elif isinstance(args[0], Bruch):
+            part += float(Bruch(self.zaehler, self.nenner)) + float(Bruch(args[0].zaehler, args[0].nenner))
+        else:
+            for i in len(args):
+                if isinstance(t,int):
+                    part += float(Bruch(self.zaehler, self.nenner)) + float(args)
+                elif isinstance(t,Bruch):
+                    t = args[i]
+                    part += float(Bruch(self.zaehler, self.nenner)) + float(Bruch(t.zaehler, t.nenner))
+        return part
+
+    def __eq__(self, other):
+        return float(self) == float(other)
+
+    def __ge__(self, other):
+        return float(self) >= float(other)
+
+    def __gt__(self, other):
+        return float(self) > float(other)
+
+    def __le__(self, other):
+        return float(self) <= float(other)
+
+    def __lt__(self, other):
+        return float(self) < float(other)
+
+    def __ne__(self, other):
+        return float(self) != float(other)
+
